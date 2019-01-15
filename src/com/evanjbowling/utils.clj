acca1472457d634bad73a0af5203b62ded9d8bae
Evan Bowling <evan.j.bowling@gmail.com>
2019-01-14 23:05:08 -0600

    Add p for pretty printing

--- src/com/evanjbowling/utils.clj
+++ src/com/evanjbowling/utils.clj
@@ -1,9 +1,19 @@
 (ns com.evanjbowling.utils
   (:require
-    [clojure.pprint :as pp]))
+    [clojure.pprint]
+    [clojure.walk]))
 
 (defn ls
   "List the files similar to unix ls command."
   []
-  (pp/pprint (file-seq (java.io.File. "."))))
+  (clojure.pprint/pprint (file-seq (java.io.File. "."))))
 
+(defn p
+  "Pretty print and sort keys of maps"
+  [x]
+  (let [sort-fn (fn [y]
+                  (if (map? y)
+                    (into (sorted-map) y)
+                    y))]
+    (clojure.pprint/pprint
+      (clojure.walk/postwalk sort-fn x))))
