48cdbcd90cc2e2d78186aa14b70e020b7d39ed78
Evan Bowling <evan.j.bowling@gmail.com>
2019-02-16 14:13:11 -0600

    Update project.clj and add cljfmt

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject com.evanjbowling.utils "0.1.0-SNAPSHOT"
-  :description "A Collection of utility methods."
-  #_#_:url "http://example.com/FIXME"
+  :description "Personal collection of clojure utility methods."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins      [[lein-cljfmt         "0.6.4"]]
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot com.evanjbowling.utils
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
