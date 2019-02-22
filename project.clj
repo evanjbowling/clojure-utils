d341af67d49323372ba902bc0c8561518c81fea7
Evan Bowling <evan.j.bowling@gmail.com>
2019-02-22 09:08:15 -0600

    Add warns for reflection in project.clj

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :target-path "target/%s"
   :repl-options {:init-ns com.evanjbowling.utils}
+  :global-vars {*warn-on-reflection* true}
   :profiles {:uberjar {:aot :all}})
